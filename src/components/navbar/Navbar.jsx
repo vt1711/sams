3c6bc9c931627441eb274cba280f9a1b75612eb1
V7777 <vedantt7777@gmail.com>
2022-10-30 09:52:37 +0530

    fixed login options toggle error

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,54 +1,60 @@
-import React ,{useContext}from 'react';
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import './navbar.css';
-import { UserContext } from '../../App';
+import "./navbar.css";
+import { UserContext } from "../../App";
 
 const Navbar = () => {
-  const MenuItems =()=>{
-    const {state,dispatch} = useContext(UserContext); 
-    if(state===false){
-      return(
-            <>
-              
-            </>
-      )
-    }
-    else{
-      return(
+  const MenuItems = () => {
+    const { state, dispatch } = useContext(UserContext);
+    if (state === true) {
+      return (
         <>
-           <NavLink
-                to='/viewrecords/graphicalview'
-                className={({ isActive }) => "navlinks" + ( isActive ? " activenavlink " : "")}
-              >View Records</NavLink>
-              <NavLink
-                className={({isActive})=> "navlinks" + (isActive ? " activenavlink " : " ")}
-                to='/updaterecords/updateexisting'>Update Records</NavLink>
-            <NavLink
-                className={({isActive})=> "navlinks" + (isActive ? " activenavlink " : " ")}
-                to='/logout'>Logout</NavLink> 
+          <NavLink
+            to="/viewrecords/graphicalview"
+            className={({ isActive }) =>
+              "navlinks" + (isActive ? " activenavlink " : "")
+            }
+          >
+            View Records
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              "navlinks" + (isActive ? " activenavlink " : " ")
+            }
+            to="/updaterecords/updateexisting"
+          >
+            Update Records
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              "navlinks" + (isActive ? " activenavlink " : " ")
+            }
+            to="/logout"
+          >
+            Logout
+          </NavLink>
         </>
-      )
+      );
+    } else {
+      return (
+        <></>
+      );
     }
-  }
+  };
   return (
     <>
       <div className="navbar">
-        
-          <div >
-            <NavLink
-              to='/'
-              className="navlogo"
-            >SAMS</NavLink>
-          </div>
-          <div className="navoptions">
-              <MenuItems/>
-          </div>
-
-        
-
+        <div>
+          <NavLink to="/" className="navlogo">
+            SAMS
+          </NavLink>
+        </div>
+        <div className="navoptions">
+          <MenuItems />
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
